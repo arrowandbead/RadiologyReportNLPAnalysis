3ce406b02540a6fd95a489b916a5bef2dc06ec6b
Malcolm Meyerson <malcolmmeyerson@Malcolms-MacBook-Pro.local>
2020-11-22 14:03:46 -0500

    wrote getFieldsFromXlsx function

--- preprocessingCode/main.py
+++ preprocessingCode/main.py
@@ -1,5 +1,1 @@
 import utilFunctions
-
-print(utilFunctions.iterateThroughFilesInFolder("../data/rawData/RMS/reports"))
-
-utilFunctions.getClinicalInformationDataMap("../data/rawData/RMS/clinical information.xlsx")
--- preprocessingCode/utilFunctions.py
+++ preprocessingCode/utilFunctions.py
@@ -4,12 +4,19 @@ import os
 def iterateThroughFilesInFolder(folderPath):
     return os.listdir(folderPath)
 
-def getClinicalInformationDataMap(clinicalInfoPath):
-    workbook = load_workbook(clinicalInfoPath)
+def getFieldsFromXlsx(filePath, fields):
+    workbook = load_workbook(filePath)
     sheet1 = workbook.get_sheet_by_name("clinical")
-    i = 1
-    for row in sheet1.iter_rows():
-        i+=1
-        print([cell.value for cell in row])
-        if i == 10:
-            exit()
+    iter = sheet1.iter_rows()
+    row0 = [cell.value for cell in next(iter)]
+    fieldToIndexMap = {}
+    for index, thing in enumerate(row0):
+        fieldToIndexMap[thing] = index
+    outPutList = []
+    for row in iter:
+        newDict = {}
+        rowAsList = [cell.value for cell in row]
+        for thing in fields:
+            newDict[thing] = rowAsList[fieldToIndexMap[thing]]
+        outPutList.append(newDict)
+    return outPutList
