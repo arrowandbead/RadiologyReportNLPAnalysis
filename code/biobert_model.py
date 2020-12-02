23af8c5c920493bfab4bf91dcb32de8c29ee7393
Alex Rodriguez <48000978+alexrodriguez0@users.noreply.github.com>
2020-12-01 23:50:34 -0400

    implement biobert model with one dense layer

--- code/biobert_model.py
+++ code/biobert_model.py
@@ -1,49 +1,103 @@
 from transformers import AutoTokenizer, TFAutoModel
 import tensorflow as tf
 import numpy as np
-from preprocessing import get_report_labels
+from preprocessing import get_data
 
 SEQ_LEN = 128
 
-class MSNR(tf.keras.Model):
+class MSNR():
     def __init__(self):
-        super(MSNR, self).__init__()
+        # super(MSNR, self).__init__()
 
         # Initializa model and tokenizer
         file_name = "giacomomiolo/biobert_reupload"
         self.biobert_tokenizer = AutoTokenizer.from_pretrained(file_name)
         self.biobert = TFAutoModel.from_pretrained(file_name)
-        self.labels = get_report_labels()
-        self.num_examples = len(self.labels)
-        self.input_ids = np.zeros((self.num_examples, SEQ_LEN))
-        self.input_mask = 
 
+        # class variables
+        self.num_labels = 7
+        self.impressions, self.labels = get_data()
+
+        # Input and layers
+        self.dense_layer = tf.keras.layers.Dense(self.num_labels, activation='softmax', name='outputs')  # adjust based on number of sentiment classes
+        self.input_ids = tf.keras.layers.Input(shape=(SEQ_LEN,), name='input_ids', dtype='int32')
+        self.mask = tf.keras.layers.Input(shape=(SEQ_LEN,), name='attention_mask', dtype='int32')
+
+        # Model variables
+        self.optimizer = tf.keras.optimizers.Adam(0.01)
+        self.loss = tf.keras.losses.CategoricalCrossentropy()
+        self.accuracy = tf.keras.metrics.CategoricalAccuracy('accuracy')
     
-    def tokenize_sentences(self, sentence):
+    def tokenize(self, impression):
         """
             Input: sentence as string
             Output: tokenized sentence array (including special tokens) and attention mask as array (1s for word and 0 o.w.)
         """
-        tokens = self.biobert_tokenizer.encode_plus(sentence, max_length=SEQ_LEN,
+        tokens = self.biobert_tokenizer.encode_plus(impression, max_length=SEQ_LEN,
                                    truncation=True, padding='max_length',
                                    add_special_tokens=True, return_attention_mask=True,
                                    return_token_type_ids=False, return_tensors='tf')
 
         return tokens['input_ids'], tokens['attention_mask']
 
-    def one_hot_encode_labels(self):
-        one_hot_labels = np.zeros((len(self.labels), 7))
-        pass
-        
-    def call(self, inputs):   
-        embeddings = self.get_embeddings(inputs)
+    def get_biobert_embeddings(self):
+        """
+            Input: None
+            Output: biobert embeddings of impressions
+        """
+        # one hot encode labels
+        encoded_labels = np.zeros((len(self.impressions), self.num_labels))
+        encoded_labels[np.arange(len(self.impressions)), np.array(self.labels) - 1] = 1
 
-        sentenceEmbeddings = self.encoder(embeddings)
+        # initialize two arrays for input tensors and tokenize impressions
+        Xids = np.zeros((len(self.impressions), SEQ_LEN))
+        Xmask = np.zeros((len(self.impressions), SEQ_LEN))
+        for i, impression in enumerate(self.impressions):
+            Xids[i, :], Xmask[i, :] = self.tokenize(impression)
 
-        aggregate_embeddings = self.layer2(sentenceEmbeddings)
+        # create TF dataset object
+        dataset = tf.data.Dataset.from_tensor_slices((Xids, Xmask, encoded_labels))
+        dataset = dataset.map(self.map_func)  # apply the mapping function
+
+        # shuffle and batch dataset
+        dataset = dataset.shuffle(454).batch(32)
+
+        # create train and test sets
+        # train_data = dataset.take(round(len(self.impressions) * 0.8))
+        # test_data = dataset.skip(round(len(self.impressions) * 0.8))
+
+        # # free space
+        # del dataset
+
+        return self.biobert(self.input_ids, attention_mask=self.mask)[0], dataset
+
+    def map_func(self, input_ids, masks, labels):
+        """
+            Purpose: structure data for input to BERT
+            Input: input_id, mask and encoded label arrays from our impressions and annotations
+            Output: tuple of (dictionary mapping name to array, labels)
+        """
+        return {'input_ids': input_ids, 'attention_mask': masks}, labels
+  
+
+    def train_model(self):   
+        """
+            Purpose: Instantiates and trains the model
+            Input: None
+            Output: None
+        """
+        embeddings, data = self.get_biobert_embeddings()
+        outputs = self.dense_layer(embeddings)
+        model = tf.keras.Model(inputs=[self.input_ids, self.mask], outputs=outputs)
         
-        prbs = self.dense(aggregate_embeddings)
-        
+        # freeze the BERT layer
+        model.layers[2].trainable = False
+
+        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.accuracy])
+
+        # and train it
+        history = model.fit(data, epochs=20)
+        print(history)
     
     def get_embeddings(self, sentences):
         """
@@ -55,3 +109,13 @@ class MSNR(tf.keras.Model):
         hidden_states = outputs[2]
         embeddings = hidden_states[0]
         return embeddings
+
+
+
+def main():
+
+    m = MSNR()
+    print(m.train_model())
+
+if __name__ == '__main__':
+    main()
--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,4 +1,4 @@
-from openpyxl import load_workbook
+# from openpyxl import load_workbook
 import os
 import pandas as pd
 import re
@@ -7,62 +7,62 @@ import numpy as np
 import collections
 import unicodedata
 
-def iterateThroughFilesInFolder(folderPath):
-    return os.listdir(folderPath)
+# def iterateThroughFilesInFolder(folderPath):
+#     return os.listdir(folderPath)
 
-def getFieldsFromXlsx(filePath, fields):
-    workbook = load_workbook(filePath)
-    sheet1 = workbook.get_sheet_by_name("clinical")
-    iter = sheet1.iter_rows()
-    row0 = [cell.value for cell in next(iter)]
-    fieldToIndexMap = {}
-    for index, thing in enumerate(row0):
-        fieldToIndexMap[thing] = index
-    outPutList = []
-    for row in iter:
-        newDict = {}
-        rowAsList = [cell.value for cell in row]
-        for thing in fields:
-            newDict[thing] = rowAsList[fieldToIndexMap[thing]]
-        outPutList.append(newDict)
-    return outPutList
+# def getFieldsFromXlsx(filePath, fields):
+#     workbook = load_workbook(filePath)
+#     sheet1 = workbook.get_sheet_by_name("clinical")
+#     iter = sheet1.iter_rows()
+#     row0 = [cell.value for cell in next(iter)]
+#     fieldToIndexMap = {}
+#     for index, thing in enumerate(row0):
+#         fieldToIndexMap[thing] = index
+#     outPutList = []
+#     for row in iter:
+#         newDict = {}
+#         rowAsList = [cell.value for cell in row]
+#         for thing in fields:
+#             newDict[thing] = rowAsList[fieldToIndexMap[thing]]
+#         outPutList.append(newDict)
+#     return outPutList
 
 
-def getExtractEvaluationInformation(filePath):
-    workbook = load_workbook(filePath)
-    sheet1 = workbook.get_sheet_by_name("sheet1")
-    iter = sheet1.iter_rows()
-    row0 = [cell.value for cell in next(iter)]
+# def getExtractEvaluationInformation(filePath):
+#     workbook = load_workbook(filePath)
+#     sheet1 = workbook.get_sheet_by_name("sheet1")
+#     iter = sheet1.iter_rows()
+#     row0 = [cell.value for cell in next(iter)]
 
-    indexToFieldMap = {}
-    for index, thing in enumerate(row0):
-        indexToFieldMap[index] = thing
-    indexToFieldMap[2] = "time"
+#     indexToFieldMap = {}
+#     for index, thing in enumerate(row0):
+#         indexToFieldMap[index] = thing
+#     indexToFieldMap[2] = "time"
 
-    outPutList = []
-    for row in iter:
-        patientReportList = []
-        outPutList.append(patientReportList)
-        rowAsList = list([cell.value for cell in row])
-        currReportDict = None
-        for index,thing in enumerate(rowAsList):
-            if(index == 0 or thing == None):
-                continue
-            if "post-MR" in indexToFieldMap[index]:
-                if currReportDict != None:
-                    if "cancer status" not in currReportDict and "evidence of cancer" not in currReportDict:
-                        patientReportList = patientReportList[:-1]
-                currReportDict = {}
-                patientReportList.append(currReportDict)
-                currReportDict["post-MR"] = str(thing).strip(" ").split(',')
-            else:
+#     outPutList = []
+#     for row in iter:
+#         patientReportList = []
+#         outPutList.append(patientReportList)
+#         rowAsList = list([cell.value for cell in row])
+#         currReportDict = None
+#         for index,thing in enumerate(rowAsList):
+#             if(index == 0 or thing == None):
+#                 continue
+#             if "post-MR" in indexToFieldMap[index]:
+#                 if currReportDict != None:
+#                     if "cancer status" not in currReportDict and "evidence of cancer" not in currReportDict:
+#                         patientReportList = patientReportList[:-1]
+#                 currReportDict = {}
+#                 patientReportList.append(currReportDict)
+#                 currReportDict["post-MR"] = str(thing).strip(" ").split(',')
+#             else:
 
-                if thing != "" and "#" not in str(thing):
-                    currReportDict[indexToFieldMap[index]] = str(thing)
-        if currReportDict != None:
-            if "cancer status" not in currReportDict and "evidence of cancer" not in currReportDict:
-                patientReportList = patientReportList[:-1]
-    return outPutList
+#                 if thing != "" and "#" not in str(thing):
+#                     currReportDict[indexToFieldMap[index]] = str(thing)
+#         if currReportDict != None:
+#             if "cancer status" not in currReportDict and "evidence of cancer" not in currReportDict:
+#                 patientReportList = patientReportList[:-1]
+#     return outPutList
 
 # OPTIONAL TODO: Remove list numbers from impressions in numbered lists
 
@@ -227,6 +227,4 @@ def make_entries_integers(key_list):
 
 def get_data():
     impressions, labels = clean_dictionary_entries()
-    return (impressions, labels, len(impressions))
-
-
+    return (impressions, labels)
