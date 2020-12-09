7457838b956fd6d46f152e2da041288dade9885b
Alex Rodriguez <48000978+alexrodriguez0@users.noreply.github.com>
2020-12-08 22:36:56 -0400

    loop through mapset

--- code/biobert_model.py
+++ code/biobert_model.py
@@ -169,8 +169,10 @@ class MSNR():
         # freeze the BERT layer
         model.layers[2].trainable = False
 
-        print("labels from train:", np.array(train_data.map(self.get_labels)))
-        print("labels from train 2:", tf.data.Dataset.as_numpy(train_data.map(self.get_labels)))
+        l = np.array(train_data.map(self.get_labels))
+        print("printing labels")
+        for d in l:
+            print(d)
 
         recall_metric = MSNR.RecallMetric(train_data.map(self.get_input_ids_and_mask), np.array(train_data.map(self.get_labels)))
 
