c2f3992f137c61b39ae83721bd21a710716cae3e
Alex Rodriguez <48000978+alexrodriguez0@users.noreply.github.com>
2020-12-10 11:29:20 -0400

    dbg

--- code/other_model.py
+++ code/other_model.py
@@ -26,7 +26,7 @@ class MSNR(tf.keras.Model):
         embeddings = []
         for i in range(len(input_ids)):
             print(len(input_ids[i]))
-            print(len(input_masks[i])
+            print(len(input_masks[i]))
             embeddings.append(self.biobert(input_ids[i], attention_mask=input_masks[i])[0])
         X = tf.keras.layers.GlobalMaxPool1D()(embeddings)  # reduce tensor dimensionality
         X = tf.keras.layers.BatchNormalization()(X)
